26a87d85deefb494b049724207ec1bd42ca8f745
Kyle <775207318@qq.com>
2020-08-26 15:15:31 +0800

    计数器demo

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, WhiteSpace, WingBlank } from 'antd-mobile';
 
 
-class Home extends React.Component {
+// class Home extends React.Component {
 
 
-    render(){
-        return (
-            <div>
-                <Button type='primary' loading size="large" style={{width:"80%",margin:'0 auto'}}>Home</Button>
-            </div>
-        )
+//     render(){
+//         return (
+//             <div>
+//                 <Button type='primary' loading size="large" style={{width:"80%",margin:'0 auto'}} >Increment</Button>
+//             </div>
+//         )
+//     }
+
+// }
+
+function Home(){
+    const [counter,setCounter] = useState(0)
+
+    const increment = () => {
+        setCounter(prevCounter => prevCounter + 1)
     }
 
-}
+    return (
+        <div>
+            <Button onClick={increment}>Increment</Button>
+            value: {counter}
+        </div>
+    )
+} 
+
 
 export default Home;
